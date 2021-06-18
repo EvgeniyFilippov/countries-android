58134a0211d9bb9092a28611f66854707dcdc4c7
Yauhen Filipau <6479556@gmail.com>
2021-06-19 01:55:14 +0300

    add first OnClickListener

--- app/src/main/java/com/example/course_android/StartFragment.kt
+++ app/src/main/java/com/example/course_android/StartFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.course_android.R
+import kotlinx.android.synthetic.main.fragment_start.*
 
 
 class StartFragment : Fragment() {
@@ -22,7 +20,9 @@ class StartFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        main_btn.setOnClickListener {
             (activity as MainActivity).navController.navigate(R.id.action_startFragment_to_secondFragment)
+        }
     }
 
 }
