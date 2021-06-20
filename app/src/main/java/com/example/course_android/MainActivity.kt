236e4b82f8dc95bb4c18ccd056dff413fc4ae194
Yauhen Filipau <6479556@gmail.com>
2021-06-20 13:37:50 +0300

    delete recycle

--- app/src/main/java/com/example/course_android/MainActivity.kt
+++ app/src/main/java/com/example/course_android/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.course_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
