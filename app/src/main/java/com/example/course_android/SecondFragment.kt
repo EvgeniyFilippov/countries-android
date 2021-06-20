9b6a50b02ac5c3d922ba904fb7807954c14ad8fd
Yauhen Filipau <6479556@gmail.com>
2021-06-20 13:38:04 +0300

    add recycle

--- app/src/main/java/com/example/course_android/SecondFragment.kt
+++ app/src/main/java/com/example/course_android/SecondFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,10 @@ class SecondFragment : Fragment() {
         back_btn.setOnClickListener {
             (activity as MainActivity).navController.navigate(R.id.action_secondFragment_to_startFragment)
         }
+        layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
     }
 }
