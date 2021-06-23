77d513ef47130db096a13f01027db9aa22df0da4
Yauhen Filipau <6479556@gmail.com>
2021-06-23 11:39:34 +0300

    delete import

--- app/src/main/java/com/example/course_android/RecyclerAdapter.kt
+++ app/src/main/java/com/example/course_android/RecyclerAdapter.kt
@@ -3,12 +3,8 @@ package com.example.course_android
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.widget.ImageViewCompat
-import androidx.core.widget.TextViewCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
