d52f4fd43f016b6df853181ddfda7d5b0f2f12ec
Yauhen Filipau <6479556@gmail.com>
2021-06-26 12:56:55 +0300

    add ViewBinding

--- app/src/main/java/com/example/course_android/MainActivity.kt
+++ app/src/main/java/com/example/course_android/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.example.course_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         navController = Navigation.findNavController(this, R.id.nav_host)
     }
 }
