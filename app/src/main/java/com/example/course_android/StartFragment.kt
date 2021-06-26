8396c6cb8b60824e1955c90ddb83a69dfc9f6e2c
Yauhen Filipau <6479556@gmail.com>
2021-06-26 17:02:07 +0300

    update

--- app/src/main/java/com/example/course_android/StartFragment.kt
+++ app/src/main/java/com/example/course_android/StartFragment.kt
@@ -8,32 +8,21 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.course_android.databinding.FragmentStartBinding
 
-class StartFragment : Fragment() {
+class StartFragment : Fragment(R.layout.fragment_start) {
 
-    private var _binding: FragmentStartBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentStartBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
+    private var binding: FragmentStartBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentStartBinding.bind(view)
-        binding.btnMain.setOnClickListener {
+        binding = FragmentStartBinding.bind(view)
+        binding?.btnMain?.setOnClickListener {
             findNavController().navigate(R.id.action_startFragment_to_secondFragment)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 
 }
