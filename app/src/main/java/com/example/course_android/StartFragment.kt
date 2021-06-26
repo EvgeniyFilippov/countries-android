2c67f6f332bbe96406416b41a44a55f5ad62d2ac
Yauhen Filipau <6479556@gmail.com>
2021-06-26 12:57:11 +0300

    add ViewBinding

--- app/src/main/java/com/example/course_android/StartFragment.kt
+++ app/src/main/java/com/example/course_android/StartFragment.kt
@@ -4,26 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.course_android.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
 
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btn_main)
-        button.setOnClickListener {
+        _binding = FragmentStartBinding.bind(view)
+        binding.btnMain.setOnClickListener {
             findNavController().navigate(R.id.action_startFragment_to_secondFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
