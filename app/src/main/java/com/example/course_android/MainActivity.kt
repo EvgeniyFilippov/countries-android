aa48aa1f280c0e0a631b2ad4918c64740767b1b5
Yauhen Filipau <6479556@gmail.com>
2021-06-27 22:25:29 +0300

    remove retrofit method

--- app/src/main/java/com/example/course_android/MainActivity.kt
+++ app/src/main/java/com/example/course_android/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.course_android.model.CountriesDataItem
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,35 +14,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var navController: NavController
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl("https://restcountries.eu/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
-    }
-
-//    https://newsapi.org/v2/top-headlines?country=us&apiKey=fe27628816ba4ca5b23fe932cf36e26e
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = Navigation.findNavController(this, R.id.nav_host)
-
-        val countriesApi = retrofit.create(CountriesApi::class.java)
-        val countriesApiCall = countriesApi.getTopHeadlines()
-
-        countriesApiCall.enqueue(object : Callback<List<CountriesDataItem>?> {
-            override fun onResponse(
-                call: Call<List<CountriesDataItem>?>,
-                response: Response<List<CountriesDataItem>?>
-            ) {
-                Log.d("RETROFIT_COUNTRIES", response.body().toString())
-            }
-
-            override fun onFailure(call: Call<List<CountriesDataItem>?>, t: Throwable) {
-                Log.d("RETROFIT_COUNTRIES", t.toString())
-            }
-        })
     }
+
 }
