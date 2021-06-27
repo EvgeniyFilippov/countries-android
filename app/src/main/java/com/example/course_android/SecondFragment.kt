728e9e85e3c15376ad2ef8dc98a0557c45aba667
Yauhen Filipau <6479556@gmail.com>
2021-06-27 22:26:08 +0300

    add retrofit method

--- app/src/main/java/com/example/course_android/SecondFragment.kt
+++ app/src/main/java/com/example/course_android/SecondFragment.kt
@@ -1,17 +1,32 @@
 package com.example.course_android
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.course_android.model.CountriesDataItem
 import kotlinx.android.synthetic.main.fragment_second.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SecondFragment : Fragment() {
 
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    lateinit var myAdapter: MyAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://restcountries.eu/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +38,33 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        adapter = RecyclerAdapter()
-        recyclerView.adapter = adapter
+
+        recyclerView.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = linearLayoutManager
+        getMyData()
+    }
+
+    private fun getMyData() {
+        val countriesApi = retrofit.create(CountriesApi::class.java)
+        val countriesApiCall = countriesApi.getTopHeadlines()
+
+        countriesApiCall.enqueue(object : Callback<List<CountriesDataItem>?> {
+            override fun onResponse(
+                call: Call<List<CountriesDataItem>?>,
+                response: Response<List<CountriesDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+                myAdapter = MyAdapter(this, responseBody)
+                myAdapter.notifyDataSetChanged()
+                recyclerView.adapter = myAdapter
+                Log.d("RETROFIT_COUNTRIES", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<List<CountriesDataItem>?>, t: Throwable) {
+                Log.d("RETROFIT_COUNTRIES", t.toString())
+            }
+        })
     }
 
 }
