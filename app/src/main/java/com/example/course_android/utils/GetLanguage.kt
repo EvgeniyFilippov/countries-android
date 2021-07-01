0796c16b50aefa0c4c8baae51ed7470502fa6b45
Yauhen Filipau <6479556@gmail.com>
2021-07-01 16:35:12 +0300

    update index

--- app/src/main/java/com/example/course_android/utils/GetLanguage.kt
+++ app/src/main/java/com/example/course_android/utils/GetLanguage.kt
@@ -5,7 +5,7 @@ import com.example.course_android.model.CountriesDataItem
 fun getLanguageByKey(countriesList: List<CountriesDataItem>, position: Int): StringBuilder {
     val myStringBuilder = StringBuilder()
     for (n in countriesList[position].languages.indices) {
-        myStringBuilder.append(countriesList[position].languages.get(n).name)
+        myStringBuilder.append(countriesList[position].languages[n].name)
         if (n < countriesList[position].languages.size - 1) {
             myStringBuilder.append(", ")
         }
