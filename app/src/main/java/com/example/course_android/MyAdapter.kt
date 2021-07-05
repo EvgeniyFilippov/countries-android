3565bedd065cc9ee9e0c2d65102a12b654bffe3d
Yauhen Filipau <6479556@gmail.com>
2021-07-05 16:27:32 +0300

    change population on area

--- app/src/main/java/com/example/course_android/MyAdapter.kt
+++ app/src/main/java/com/example/course_android/MyAdapter.kt
@@ -24,21 +24,21 @@ class MyAdapter(val context: Callback<List<CountriesDataItem>?>, private val cou
         holder.itemTitle.text = countriesList[position].name
         holder.itemDetail.text = countriesList[position].capital
         holder.itemLang.text = countriesList[position].languages.getLanguageByKey()
-        holder.itemPopulation.text = countriesList[position].population.toString()
+        holder.itemArea.text = countriesList[position].area.toString()
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         var itemTitle: AppCompatTextView
         var itemDetail: AppCompatTextView
         var itemLang: AppCompatTextView
-        var itemPopulation: AppCompatTextView
+        var itemArea: AppCompatTextView
 
 
         init {
             itemTitle = itemView.findViewById(R.id.item_title)
             itemDetail = itemView.findViewById(R.id.item_detail)
             itemLang = itemView.findViewById(R.id.item_lang)
-            itemPopulation = itemView.findViewById(R.id.item_population)
+            itemArea = itemView.findViewById(R.id.item_area)
         }
     }
 }
