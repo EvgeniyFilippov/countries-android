8707fad68c729d13376908202256d58d2a15af6d
Yauhen Filipau <6479556@gmail.com>
2021-07-06 14:39:00 +0300

    fix add languages

--- app/src/main/java/com/example/course_android/room/LanguagesInfoEntity.kt
+++ app/src/main/java/com/example/course_android/room/LanguagesInfoEntity.kt
@@ -4,9 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "languages_table")
+@Entity(tableName = "languages_table", primaryKeys = ["countryName","language"])
 class LanguagesInfoEntity (
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "auto_id") val id: Int,
+//    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "auto_id") val id: Int,
     @ColumnInfo val countryName: String,
     @ColumnInfo val language: String
 )
+
