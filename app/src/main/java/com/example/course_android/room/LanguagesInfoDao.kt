b7f7e908f43a242f2bc014d63a6f32b77db6b892
Yauhen Filipau <6479556@gmail.com>
2021-07-06 14:39:04 +0300

    fix add languages

--- app/src/main/java/com/example/course_android/room/LanguagesInfoDao.kt
+++ app/src/main/java/com/example/course_android/room/LanguagesInfoDao.kt
@@ -1,11 +1,19 @@
 package com.example.course_android.room
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface LanguagesInfoDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun add(entity: LanguagesInfoEntity)
+
+    @Query("SELECT * FROM languages_table")
+    fun getAllInfo(): List<LanguagesInfoEntity>
+
+    @Query("SELECT language FROM languages_table WHERE countryName = :name")
+    fun getLanguageByCountry(name: String): List<String>
 }
