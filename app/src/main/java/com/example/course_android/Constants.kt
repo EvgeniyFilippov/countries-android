385e6285b37551707c41da59fe55059e3983d152
Yauhen Filipau <6479556@gmail.com>
2021-07-07 13:28:52 +0300

    add constants object

---
+++ app/src/main/java/com/example/course_android/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.course_android
+
+object Constants {
+    const val BASE_URL = "https://restcountries.eu/"
+    const val DEFAULT_INT = 0
+    const val KEY_SORT_STATUS = "sortStatus"
+    const val FILE_NAME_PREF = "data"
+    const val DEFAULT_STRING = ""
+}
