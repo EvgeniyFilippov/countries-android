e4664f95914550a453a313bed580485cc1f90617
Yauhen Filipau <6479556@gmail.com>
2021-07-07 15:37:24 +0300

    add converter

---
+++ app/src/main/java/com/example/course_android/utils/CovertDBdataToRetrofitModel.kt
@@ -0,0 +1,22 @@
+package com.example.course_android.utils
+
+import com.example.course_android.Constants
+import com.example.course_android.model.CountriesDataItem
+import com.example.course_android.model.Language
+import com.example.course_android.room.CountryBaseInfoEntity
+import com.example.course_android.room.LanguagesInfoDAO
+
+fun  List<CountryBaseInfoEntity>?.convertDBdataToRetrofitModel(
+    languagesFromDB: LanguagesInfoDAO?, listOfCountriesFromDB: MutableList<CountriesDataItem>
+): MutableList<CountriesDataItem> {
+    this?.forEach{countryDB ->
+        val listOfLanguagesFromDB: MutableList<Language> = mutableListOf()
+        languagesFromDB?.getLanguageByCountry(countryDB.name)?.forEach{languageDB ->
+            val languageItem = Language(Constants.DEFAULT_STRING, Constants.DEFAULT_STRING, languageDB, Constants.DEFAULT_STRING)
+            listOfLanguagesFromDB.add(languageItem)
+        }
+        val countryDataItem = CountriesDataItem(countryDB.area, countryDB.capital, listOfLanguagesFromDB, countryDB.name)
+        listOfCountriesFromDB.add(countryDataItem)
+    }
+    return listOfCountriesFromDB
+}
