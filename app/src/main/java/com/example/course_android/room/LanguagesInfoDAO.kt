0368b6b260db77addb9673fee8764370aa5a8f63
Yauhen Filipau <6479556@gmail.com>
2021-07-07 21:55:24 +0300

    add addAll method

--- app/src/main/java/com/example/course_android/room/LanguagesInfoDAO.kt
+++ app/src/main/java/com/example/course_android/room/LanguagesInfoDAO.kt
@@ -16,4 +16,7 @@ interface LanguagesInfoDAO {
 
     @Query("SELECT language FROM languages_table WHERE countryName = :name")
     fun getLanguageByCountry(name: String): List<String>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addAll(list: List<LanguagesInfoEntity>)
 }
