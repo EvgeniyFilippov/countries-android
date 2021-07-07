d3a293ab2ff35fd8122d5cc56af75fcd47868acf
Yauhen Filipau <6479556@gmail.com>
2021-07-07 19:57:30 +0300

    refactoring

--- app/src/main/java/com/example/course_android/room/LanguagesInfoEntity.kt
+++ app/src/main/java/com/example/course_android/room/LanguagesInfoEntity.kt
@@ -4,9 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "languages_table", primaryKeys = ["countryName","language"])
-class LanguagesInfoEntity (
-//    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "auto_id") val id: Int,
+@Entity(tableName = "languages_table", primaryKeys = ["countryName", "language"])
+class LanguagesInfoEntity(
     @ColumnInfo val countryName: String,
     @ColumnInfo val language: String
 )
