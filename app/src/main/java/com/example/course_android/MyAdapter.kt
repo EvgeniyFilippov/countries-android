b8dc03e44d4a06ca41228b68c12cf4c69905178e
Yauhen Filipau <6479556@gmail.com>
2021-07-09 01:39:12 +0300

    update

--- app/src/main/java/com/example/course_android/MyAdapter.kt
+++ app/src/main/java/com/example/course_android/MyAdapter.kt
@@ -5,39 +5,60 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.course_android.base.adapter.BaseAdapter
 import com.example.course_android.model.CountriesDataItem
 import com.example.course_android.utils.getLanguageByKey
 
-class MyAdapter(private val countriesList: List<CountriesDataItem>): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+class MyAdapter : BaseAdapter<CountriesDataItem>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.ViewHolder {
+    class CountryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val tvCountry: AppCompatTextView = view.findViewById(R.id.item_title)
+        val tvCapital: AppCompatTextView = view.findViewById(R.id.item_detail)
+        val tvLanguages: AppCompatTextView = view.findViewById(R.id.item_lang)
+        val tvArea: AppCompatTextView = view.findViewById(R.id.item_area)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
-        return ViewHolder(itemView)
+        return CountryViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        return countriesList.size
-    }
-
-    override fun onBindViewHolder(holder: MyAdapter.ViewHolder, position: Int) {
-        holder.itemTitle.text = countriesList[position].name
-        holder.itemDetail.text = countriesList[position].capital
-        holder.itemLang.text = countriesList[position].languages.getLanguageByKey()
-        holder.itemArea.text = countriesList[position].area.toString()
-    }
-
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var itemTitle: AppCompatTextView
-        var itemDetail: AppCompatTextView
-        var itemLang: AppCompatTextView
-        var itemArea: AppCompatTextView
-
-
-        init {
-            itemTitle = itemView.findViewById(R.id.item_title)
-            itemDetail = itemView.findViewById(R.id.item_detail)
-            itemLang = itemView.findViewById(R.id.item_lang)
-            itemArea = itemView.findViewById(R.id.item_area)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is CountryViewHolder) {
+            val item = mDataList[position]
+            holder.tvCountry.text = item.name
+            holder.tvCapital.text = item.capital
+            holder.tvLanguages.text = item.languages.getLanguageByKey()
+            holder.tvArea.text = item.area.toString()
+            holder.itemView.setOnClickListener{ mClickFunction?.invoke(item)}
         }
     }
+
+//    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//        var itemTitle: AppCompatTextView
+//        var itemDetail: AppCompatTextView
+//        var itemLang: AppCompatTextView
+//        var itemArea: AppCompatTextView
+//
+//
+//        init {
+//            itemTitle = itemView.findViewById(R.id.item_title)
+//            itemDetail = itemView.findViewById(R.id.item_detail)
+//            itemLang = itemView.findViewById(R.id.item_lang)
+//            itemArea = itemView.findViewById(R.id.item_area)
+//        }
+//    }
+
+    fun sortAndReplaceItem() {
+        mDataList.sortBy { it.area }
+        notifyDataSetChanged()
+    }
+
+    fun sortDescendingAndReplaceItem() {
+        mDataList.sortByDescending { it.area }
+        notifyDataSetChanged()
+    }
+
+
 }
