f8b184db75d463487714aa3cf0dafe0b08477f2a
Yauhen Filipau <6479556@gmail.com>
2021-07-10 19:24:08 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/CountryDetailsFragment.kt
+++ app/src/main/java/com/example/course_android/fragments/CountryDetailsFragment.kt
@@ -1,22 +1,29 @@
 package com.example.course_android.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.course_android.Constants
-import com.example.course_android.R
 import com.example.course_android.databinding.FragmentCountryDetailsBinding
+import com.example.course_android.model.Language
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
 
 class CountryDetailsFragment : Fragment() {
 
     private lateinit var mCountryName: String
+    private lateinit var mLanguageJsonString: String
     private lateinit var binding: FragmentCountryDetailsBinding
+    private var mLanguageList: MutableList<Language>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mCountryName = arguments?.getString(Constants.COUNTRY_NAME_KEY) ?: Constants.ERROR
+        mLanguageJsonString = arguments?.getString(Constants.LANGUAGES_LIST) ?: Constants.ERROR
+//        val mLanguageList = getGsonParser()?.fromJson(mLanguageJsonString, Language::class.java)
     }
 
     override fun onCreateView(
@@ -30,5 +37,18 @@ class CountryDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mTvCountryName.text = mCountryName
+        binding.mTvLanguageJsonString.text = mLanguageJsonString
+    }
+
+    companion object {
+        private var gson: Gson? = null
+
+        private fun getGsonParser(): Gson? {
+            if (null == gson) {
+                val builder = GsonBuilder()
+                gson = builder.create()
+            }
+            return gson
+        }
     }
 }
