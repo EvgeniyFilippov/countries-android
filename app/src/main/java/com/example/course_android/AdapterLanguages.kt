628a83c905d078ed0b49bd3e6efad92a43d4802d
Yauhen Filipau <6479556@gmail.com>
2021-07-10 22:20:59 +0300

    add adapter

---
+++ app/src/main/java/com/example/course_android/AdapterLanguages.kt
@@ -0,0 +1,32 @@
+package com.example.course_android
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.course_android.base.adapter.BaseAdapter
+import com.example.course_android.model.Language
+
+class AdapterLanguages : BaseAdapter<Language>() {
+
+    class LanguageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val tvLanguages: AppCompatTextView = view.findViewById(R.id.item_lang)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LanguageViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_language
+                , parent, false)
+        return LanguageViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is LanguageViewHolder) {
+            val item = mDataList[position]
+            holder.tvLanguages.text = item.name
+        }
+    }
+
+}
