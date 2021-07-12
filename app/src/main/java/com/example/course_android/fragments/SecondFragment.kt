6159300751ecf12b793ac516c6cd2e63f3d658bb
Yauhen Filipau <6479556@gmail.com>
2021-07-12 22:27:44 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/course_android/fragments/SecondFragment.kt
@@ -18,7 +18,7 @@ import com.example.course_android.R
 import com.example.course_android.api.CountriesApi
 import com.example.course_android.api.RetrofitObj
 import com.example.course_android.databinding.FragmentSecondBinding
-import com.example.course_android.model.CountriesDataItem
+import com.example.course_android.model.allCountries.CountriesDataItem
 import com.example.course_android.room.*
 import com.example.course_android.utils.convertDBdataToRetrofitModel
 import com.example.course_android.utils.sortBySortStatusFromPref
@@ -136,10 +136,7 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
 
                     myAdapter.setItemClick { item ->
                         val bundle = Bundle()
-                        val gson = GsonBuilder().create()
                         bundle.putString(Constants.COUNTRY_NAME_KEY, item.name)
-                        bundle.putString(Constants.LANGUAGES_LIST_KEY, gson.toJson(item.languages))
-                        bundle.putString(Constants.COUNTRY_FLAG_KEY, item.flag)
                         findNavController().navigate(
                             R.id.action_secondFragment_to_countryDetailsFragment,
                             bundle
