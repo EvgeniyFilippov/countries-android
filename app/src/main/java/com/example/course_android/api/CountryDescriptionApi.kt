9785ef76638a7e6db594a0de7ed0bd3ba51c445f
Yauhen Filipau <6479556@gmail.com>
2021-07-12 22:40:20 +0300

    add Path

--- app/src/main/java/com/example/course_android/api/CountryDescriptionApi.kt
+++ app/src/main/java/com/example/course_android/api/CountryDescriptionApi.kt
@@ -3,11 +3,12 @@ package com.example.course_android.api
 import com.example.course_android.model.oneCountry.CountryDescriptionItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CountryDescriptionApi {
-    @GET("rest/v2/name/belarus")
+    @GET("rest/v2/name/{country}")
     fun getTopHeadlines(
-//        @Query("country") country: String
+        @Path("country") country: String
     ): Call<List<CountryDescriptionItem>>
 }
