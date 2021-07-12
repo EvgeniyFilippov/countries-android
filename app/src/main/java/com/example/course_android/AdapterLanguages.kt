1ab095d871037341262f8ff5601eba7936d46f6e
Yauhen Filipau <6479556@gmail.com>
2021-07-12 22:26:37 +0300

    update

--- app/src/main/java/com/example/course_android/AdapterLanguages.kt
+++ app/src/main/java/com/example/course_android/AdapterLanguages.kt
@@ -6,9 +6,10 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.course_android.base.adapter.BaseAdapter
-import com.example.course_android.model.Language
+import com.example.course_android.model.allCountries.Language
+import com.example.course_android.model.oneCountry.LanguageOfOneCountry
 
-class AdapterLanguages : BaseAdapter<Language>() {
+class AdapterLanguages : BaseAdapter<LanguageOfOneCountry>() {
 
     class LanguageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
