d8f4ecc2fb6696391928c6ad487f7e093295be91
Yauhen Filipau <6479556@gmail.com>
2021-07-13 01:53:10 +0300

    move base

--- app/src/main/java/com/example/course_android/CountriesApp.kt
+++ app/src/main/java/com/example/course_android/CountriesApp.kt
@@ -2,17 +2,25 @@ package com.example.course_android
 
 import android.app.Application
 import com.example.course_android.api.RetrofitObj
+import com.example.course_android.room.CountryInfoDAO
+import com.example.course_android.room.DatabaseInfo
+import com.example.course_android.room.LanguagesInfoDAO
 import retrofit2.Retrofit
 
 class CountriesApp : Application() {
 
     companion object {
         lateinit var retrofit: Retrofit
+        var base: DatabaseInfo? = null
+        var daoCountry: CountryInfoDAO? = null
+        var daoLanguage: LanguagesInfoDAO? = null
     }
 
     override fun onCreate() {
         super.onCreate()
         retrofit = RetrofitObj.getRetrofit(RetrofitObj.getOkHttp())
+        base = this.let { DatabaseInfo.init(it) }
+        daoCountry = base?.getCountryInfoDAO()
+        daoLanguage = base?.getLanguageInfoDAO()
     }
 }
-
