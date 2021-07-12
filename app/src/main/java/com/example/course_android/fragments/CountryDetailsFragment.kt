cc169c740082c76e7cb8d03428790d21b27aa89e
Yauhen Filipau <6479556@gmail.com>
2021-07-12 22:27:03 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/CountryDetailsFragment.kt
+++ app/src/main/java/com/example/course_android/fragments/CountryDetailsFragment.kt
@@ -1,10 +1,11 @@
 package com.example.course_android.fragments
 
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,18 +17,25 @@ import coil.request.ImageRequest
 //import coil.request.ImageRequest
 import com.example.course_android.AdapterLanguages
 import com.example.course_android.Constants
+import com.example.course_android.CountriesApp
 import com.example.course_android.R
+import com.example.course_android.api.CountryDescriptionApi
+import com.example.course_android.api.RetrofitObj
 import com.example.course_android.databinding.FragmentCountryDetailsBinding
-import com.example.course_android.model.Language
+import com.example.course_android.model.allCountries.Language
+import com.example.course_android.model.oneCountry.CountryDescriptionItem
+import com.example.course_android.model.oneCountry.LanguageOfOneCountry
 import com.google.android.libraries.maps.CameraUpdateFactory
 //import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.SupportMapFragment
-import com.google.android.libraries.maps.model.CameraPosition
 import com.google.android.libraries.maps.model.LatLng
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.fragment_country_details.*
 import kotlinx.android.synthetic.main.fragment_second.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class CountryDetailsFragment : Fragment() {
@@ -36,23 +44,19 @@ class CountryDetailsFragment : Fragment() {
     private lateinit var mCountryFlagString: String
     private lateinit var mLanguageJsonString: String
     private lateinit var binding: FragmentCountryDetailsBinding
-    private var mLanguageList: List<Language>? = null
+    private var mLanguageList: List<LanguageOfOneCountry>? = null
     lateinit var adapterLanguages: AdapterLanguages
     lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var countryDescriptionFromApi: MutableList<CountryDescriptionItem>
 
     private lateinit var googleMap: GoogleMap
-    var mapFragment : SupportMapFragment? = null
+    var mapFragment: SupportMapFragment? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mCountryName = arguments?.getString(Constants.COUNTRY_NAME_KEY) ?: Constants.ERROR
 
-        mLanguageJsonString = arguments?.getString(Constants.LANGUAGES_LIST_KEY) ?: Constants.ERROR
-        val gson = GsonBuilder().create()
-        mLanguageList = gson.fromJson(mLanguageJsonString, Array<Language>::class.java)?.toList()
-
-        mCountryFlagString = arguments?.getString(Constants.COUNTRY_FLAG_KEY) ?: Constants.ERROR
 
     }
 
@@ -67,27 +71,22 @@ class CountryDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getMyData()
+        //название страны
         binding.mTvCountryName.text = mCountryName
-//        val uri = Uri.parse(mCountryFlagString)
-//        GlideToVectorYou.justLoadImage(activity, uri, item_flag)
-        binding.itemFlag.loadSvg(mCountryFlagString)
-        mapFragment = childFragmentManager.findFragmentById(R.id.mapFragmentContainer) as? SupportMapFragment?
+
+
+
+        //карта гугл
+        mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapFragmentContainer) as? SupportMapFragment?
         mapFragment?.run {
             getMapAsync { map -> initMap(map) }
         }
 
 
-        linearLayoutManager = LinearLayoutManager(context)
-        recycler_languages.layoutManager = linearLayoutManager
-        adapterLanguages = AdapterLanguages()
 
-        val params: ViewGroup.LayoutParams = recycler_languages.layoutParams
-        params.height =
-            mLanguageList?.size?.times(Constants.LANGUAGE_VIEW_HEIGHT) ?: Constants.DEFAULT_INT
-        recycler_languages.layoutParams = params
 
-        recycler_languages.adapter = adapterLanguages
-        adapterLanguages.repopulate(mLanguageList as MutableList<Language>)
     }
 
 
@@ -99,6 +98,7 @@ class CountryDetailsFragment : Fragment() {
         }
 
     }
+
     private fun AppCompatImageView.loadSvg(url: String) {
         val imageLoader = ImageLoader.Builder(this.context)
             .componentRegistry { add(SvgDecoder(this@loadSvg.context)) }
@@ -112,5 +112,51 @@ class CountryDetailsFragment : Fragment() {
             .build()
 
         imageLoader.enqueue(request)
+    }
+
+    private fun getMyData() {
+        RetrofitObj.getOkHttp()
+        val countryDescrApi = CountriesApp.retrofit.create(CountryDescriptionApi::class.java)
+        val countryDescrApiCall = countryDescrApi.getTopHeadlines()
+
+        countryDescrApiCall.enqueue(object : Callback<List<CountryDescriptionItem>?> {
+            override fun onResponse(
+                call: Call<List<CountryDescriptionItem>?>,
+                response: Response<List<CountryDescriptionItem>?>
+            ) {
+                if (response.body() != null) {
+                    countryDescriptionFromApi =
+                        (response.body() as MutableList<CountryDescriptionItem>)
+
+                    //языки ресайкл
+                    linearLayoutManager = LinearLayoutManager(context)
+                    recycler_languages.layoutManager = linearLayoutManager
+                    adapterLanguages = AdapterLanguages()
+
+                    mLanguageList = countryDescriptionFromApi[0].languages
+
+                    val params: ViewGroup.LayoutParams = recycler_languages.layoutParams
+                    params.height =
+                        mLanguageList?.size?.times(Constants.LANGUAGE_VIEW_HEIGHT) ?: Constants.DEFAULT_INT
+                    recycler_languages.layoutParams = params
+
+                    recycler_languages.adapter = adapterLanguages
+                    adapterLanguages.repopulate(mLanguageList as MutableList<LanguageOfOneCountry>)
+
+                    //флаг
+                    binding.itemFlag.loadSvg(countryDescriptionFromApi[0].flag)
+
+
+
+                } else {
+                    Log.d("RETROFIT_COUNTRIES", response.body().toString())
+                }
+
+            }
+
+            override fun onFailure(call: Call<List<CountryDescriptionItem>?>, t: Throwable) {
+                Log.d("RETROFIT_COUNTRIES", t.toString())
+            }
+        })
     }
 }
