2d79674f9b1fd0fa08a173e761fabb2557d5a791
Yauhen Filipau <6479556@gmail.com>
2021-07-13 19:09:48 +0300

    add new class

---
+++ app/src/main/java/com/example/course_android/dto/model/LanguageOfOneCountryDto.kt
@@ -0,0 +1,8 @@
+package com.example.course_android.dto.model
+
+data class LanguageOfOneCountryDto (
+    var iso639_1: String = "",
+    var iso639_2: String = "",
+    var name: String = "",
+    var nativeName: String = ""
+        )
