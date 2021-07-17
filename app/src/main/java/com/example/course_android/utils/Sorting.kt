98d9cfd2f97a9cdc20f239fc922d3ea056514c30
Yauhen Filipau <6479556@gmail.com>
2021-07-17 19:39:00 +0300

    update

--- app/src/main/java/com/example/course_android/utils/Sorting.kt
+++ app/src/main/java/com/example/course_android/utils/Sorting.kt
@@ -4,9 +4,15 @@ import com.example.course_android.Constants
 import com.example.course_android.model.allCountries.CountriesDataItem
 
 fun MutableList<CountriesDataItem>.sortBySortStatusFromPref(sortStatus: Int) {
-    if (sortStatus == Constants.SORT_STATUS_UP) {
-        this.sortBy { it.area }
-    } else if (sortStatus == Constants.SORT_STATUS_DOWN) {
-        this.sortByDescending { it.area }
+    when (sortStatus) {
+        Constants.SORT_STATUS_UP -> {
+            this.sortBy { it.area }
+        }
+        Constants.SORT_STATUS_DOWN -> {
+            this.sortByDescending { it.area }
+        }
+        else -> {
+            this.sortBy {it.name}
+        }
     }
 }
