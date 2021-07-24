bdedce41e1a97af53dbdbc0d1a4f083aaa9f40b6
Yauhen Filipau <6479556@gmail.com>
2021-07-24 15:34:27 +0300

    add

---
+++ app/src/main/java/com/example/course_android/base/mvp/BaseMvpPresenter.kt
@@ -0,0 +1,45 @@
+package com.example.course_android.base.mvp
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+abstract class BaseMvpPresenter<View : BaseMvpView> {
+
+    private lateinit var mView: View
+    private val mCompositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    fun attacjView(view: View) {
+        mView = view
+    }
+
+    protected open fun getView(): View? = mView
+
+    fun onDestroyView() {
+        mCompositeDisposable.clear()
+    }
+
+    fun addDisposable(disposable: Disposable) {
+        mCompositeDisposable.add(disposable)
+    }
+
+    fun <Data> inBackground(flowable: Flowable<Data>): Flowable<Data> {
+        return  flowable.observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+    }
+
+    fun <Data> handleProgress(flowable: Flowable<Data>, isRefresh: Boolean): Flowable<Data> {
+        return flowable
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                if (!isRefresh) {
+                    getView()?.showProgress()
+                }
+            }.doOnNext {
+                getView()?.hideProgress()
+            }
+            .observeOn(Schedulers.io())
+    }
+}
