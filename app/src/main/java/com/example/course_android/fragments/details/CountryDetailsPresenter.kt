312f2cecc4e766175dc38e0717af1c0985020228
Yauhen Filipau <6479556@gmail.com>
2021-07-24 16:06:28 +0300

    update

---
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.course_android.fragments.details
+
+import com.example.course_android.base.mvp.BaseMvpPresenter
+
+class CountryDetailsPresenter : BaseMvpPresenter<CountryDetailsView>() {
+
+}
