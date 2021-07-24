9fe75c546ccf6cc2c9e367361a7123b798ef623b
Yauhen Filipau <6479556@gmail.com>
2021-07-24 15:34:17 +0300

    add

---
+++ app/src/main/java/com/example/course_android/base/mvp/BaseMvpFragment.kt
@@ -0,0 +1,17 @@
+package com.example.course_android.base.mvp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseMvpFragment<View: BaseMvpView, PresenterType : BaseMvpPresenter<View>> : Fragment() {
+    protected lateinit var mPresenter: PresenterType
+
+    abstract  fun createPresenter()
+
+    abstract fun getPresenter() : PresenterType
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createPresenter()
+    }
+}
