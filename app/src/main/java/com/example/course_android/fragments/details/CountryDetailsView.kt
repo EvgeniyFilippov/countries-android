e545731aec20b6b4e9dc2a2833b623edd89153e4
Yauhen Filipau <6479556@gmail.com>
2021-07-24 16:06:35 +0300

    update

---
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsView.kt
@@ -0,0 +1,11 @@
+package com.example.course_android.fragments.details
+
+import com.example.course_android.base.mvp.BaseMvpView
+import com.example.course_android.dto.model.CountryDescriptionItemDto
+import com.google.android.libraries.maps.model.LatLng
+
+interface CountryDetailsView : BaseMvpView {
+
+    fun showCountryInfo(country: CountryDescriptionItemDto, location: LatLng)
+
+}
