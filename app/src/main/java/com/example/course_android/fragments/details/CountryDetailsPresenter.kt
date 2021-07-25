309e5b1f358c7bf9f398e41cbe42f842c97137f3
Yauhen Filipau <6479556@gmail.com>
2021-07-25 03:13:24 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
@@ -1,7 +1,39 @@
 package com.example.course_android.fragments.details
 
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.course_android.CountriesApp
+import com.example.course_android.R
+import com.example.course_android.api.CountryDescriptionApi
+import com.example.course_android.api.RetrofitObj
 import com.example.course_android.base.mvp.BaseMvpPresenter
+import com.example.course_android.dto.CountryDetailsDtoTransformer
+import com.example.course_android.dto.model.LanguageOfOneCountryDto
+import com.example.course_android.model.oneCountry.CountryDescriptionItem
+import com.example.course_android.utils.loadSvg
+import com.google.android.libraries.maps.SupportMapFragment
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_country_details.*
+
 
 class CountryDetailsPresenter : BaseMvpPresenter<CountryDetailsView>() {
+    private val countryDetailsDtoTransformer = CountryDetailsDtoTransformer()
 
+    fun getMyData(mCountryName: String, isRefresh: Boolean) {
+        RetrofitObj.getOkHttp()
+        val countryDescrApi = CountriesApp.retrofit.create(CountryDescriptionApi::class.java)
+        addDisposable(
+            inBackground(
+                handleProgress(countryDescrApi.getTopHeadlines(mCountryName), isRefresh)
+            ).subscribe({ response ->
+
+                getView()?.showCountryInfo(
+                    countryDetailsDtoTransformer.transform(response))
+
+            }, {
+                it.message?.let { it1 -> getView()?.showError(it1, it) }
+            })
+        )
+    }
 }
