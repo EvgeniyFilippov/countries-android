a8e86d33d26d4a86f85390095fbadefb71d8f794
Yauhen Filipau <6479556@gmail.com>
2021-07-26 13:22:30 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesPresenter.kt
+++ app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesPresenter.kt
@@ -1,6 +1,25 @@
 package com.example.course_android.fragments.map
 
+import com.example.course_android.CountriesApp
+import com.example.course_android.api.CountriesApi
+import com.example.course_android.api.CountryDescriptionApi
+import com.example.course_android.api.RetrofitObj
 import com.example.course_android.base.mvp.BaseMvpPresenter
 
 class MapAllCountriesPresenter : BaseMvpPresenter<MapAllCountriesView>() {
+
+    fun getAllCountries(isRefresh: Boolean) {
+        RetrofitObj.getOkHttp()
+        val countriesApi = CountriesApp.retrofit.create(CountriesApi::class.java)
+        addDisposable(
+            inBackground(
+                handleProgress(countriesApi.getTopHeadlines(), isRefresh)
+            ).subscribe({ response ->
+                getView()?.showAllCountriesOnMap(response)
+            }, {
+                it.message?.let { it1 -> getView()?.showError(it1, it) }
+            })
+        )
+    }
+
 }
