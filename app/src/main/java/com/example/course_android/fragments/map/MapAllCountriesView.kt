8771c69cf55b89794568e137ff40b75e6caa1a50
Yauhen Filipau <6479556@gmail.com>
2021-07-26 12:48:01 +0300

    add

---
+++ app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesView.kt
@@ -0,0 +1,8 @@
+package com.example.course_android.fragments.map
+
+import com.example.course_android.base.mvp.BaseMvpView
+
+interface MapAllCountriesView : BaseMvpView {
+
+    fun showAllCountriesOnMap()
+}
