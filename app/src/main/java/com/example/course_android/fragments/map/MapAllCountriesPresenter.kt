2b0f60769e7dcccc82e4361436bc754426eddbd4
Yauhen Filipau <6479556@gmail.com>
2021-07-26 12:47:55 +0300

    add

---
+++ app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesPresenter.kt
@@ -0,0 +1,6 @@
+package com.example.course_android.fragments.map
+
+import com.example.course_android.base.mvp.BaseMvpPresenter
+
+class MapAllCountriesPresenter : BaseMvpPresenter<MapAllCountriesView>() {
+}
