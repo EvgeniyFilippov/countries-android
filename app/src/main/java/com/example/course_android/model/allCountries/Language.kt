e15d68efdaa66d5a775b81b72b2c787b6c016afb
Yauhen Filipau <6479556@gmail.com>
2021-07-27 17:38:00 +0300

    update

--- app/src/main/java/com/example/course_android/model/allCountries/Language.kt
+++
@@ -1,8 +0,0 @@
-package com.example.course_android.model.allCountries
-
-data class Language (
-    var iso639_1: String,
-    var iso639_2: String,
-    var name: String,
-    var nativeName: String
-)
