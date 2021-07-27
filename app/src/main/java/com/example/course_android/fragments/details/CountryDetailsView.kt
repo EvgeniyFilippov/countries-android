0f0e18e97d9e8c0f025a945ad81b68d643a2b72d
Yauhen Filipau <6479556@gmail.com>
2021-07-27 11:02:56 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/details/CountryDetailsView.kt
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsView.kt
@@ -2,7 +2,6 @@ package com.example.course_android.fragments.details
 
 import com.example.course_android.base.mvp.BaseMvpView
 import com.example.course_android.dto.model.CountryDescriptionItemDto
-import com.google.android.libraries.maps.model.LatLng
 
 interface CountryDetailsView : BaseMvpView {
 
