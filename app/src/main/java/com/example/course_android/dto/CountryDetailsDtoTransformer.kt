d0504d99c4920c1e86a72293eb3f555d0fdf0268
Yauhen Filipau <6479556@gmail.com>
2021-07-27 17:37:16 +0300

    update

--- app/src/main/java/com/example/course_android/dto/CountryDetailsDtoTransformer.kt
+++ app/src/main/java/com/example/course_android/dto/CountryDetailsDtoTransformer.kt
@@ -1,32 +1,38 @@
 package com.example.course_android.dto
 
+import com.example.course_android.Constants.DEFAULT_DOUBLE
 import com.example.course_android.Constants.DEFAULT_FLAG
-import com.example.course_android.Constants.DEFAULT_LATLNG
 import com.example.course_android.Constants.DEFAULT_LATLNG_SIZE
 import com.example.course_android.Constants.DEFAULT_STRING
-import com.example.course_android.Constants.FIRST_COUNTRY
 import com.example.course_android.dto.model.CountryDescriptionItemDto
 import com.example.course_android.dto.model.LanguageOfOneCountryDto
 import com.example.course_android.model.oneCountry.CountryDescriptionItem
 
 class CountryDetailsDtoTransformer :
-    Transformer<List<CountryDescriptionItem>, CountryDescriptionItemDto> {
-    override fun transform(item: List<CountryDescriptionItem>?): CountryDescriptionItemDto {
-        val countryDescriptionItemDto = CountryDescriptionItemDto()
-        val listOfLanguagesDto: MutableList<LanguageOfOneCountryDto> = mutableListOf()
+    Transformer<List<CountryDescriptionItem>, List<CountryDescriptionItemDto>> {
 
-        item?.let {
-            countryDescriptionItemDto.flag = it[FIRST_COUNTRY].flag ?: DEFAULT_FLAG
-            countryDescriptionItemDto.name = it[FIRST_COUNTRY].name ?: DEFAULT_STRING
+    override fun transform(item: List<CountryDescriptionItem>?): MutableList<CountryDescriptionItemDto> {
+
+        val listCountryDescriptionItemDto: MutableList<CountryDescriptionItemDto> = mutableListOf()
+
+
+        item?.forEach { country ->
+            val countryDescriptionItemDto = CountryDescriptionItemDto()
+            val listOfLanguagesDto: MutableList<LanguageOfOneCountryDto> = mutableListOf()
+
+            countryDescriptionItemDto.flag = country.flag ?: DEFAULT_FLAG
+            countryDescriptionItemDto.name = country.name ?: DEFAULT_STRING
+            countryDescriptionItemDto.area = country.area ?: DEFAULT_DOUBLE
+            countryDescriptionItemDto.capital = country.capital ?: DEFAULT_STRING
 
             countryDescriptionItemDto.latlng =
-                it[FIRST_COUNTRY].latlng ?: arrayListOf(DEFAULT_LATLNG, DEFAULT_LATLNG)
+                country.latlng ?: arrayListOf(DEFAULT_DOUBLE, DEFAULT_DOUBLE)
             if (countryDescriptionItemDto.latlng.size != DEFAULT_LATLNG_SIZE) {
-                countryDescriptionItemDto.latlng = arrayListOf(DEFAULT_LATLNG, DEFAULT_LATLNG)
+                countryDescriptionItemDto.latlng = arrayListOf(DEFAULT_DOUBLE, DEFAULT_DOUBLE)
             }
 
             var count = 0;
-            it[FIRST_COUNTRY].languages?.forEach {
+            country.languages?.forEach {
                 val languageDto = LanguageOfOneCountryDto()
                 languageDto.iso639_1 = it.iso639_1 ?: ""
                 languageDto.iso639_2 = it.iso639_2 ?: ""
@@ -36,7 +42,12 @@ class CountryDetailsDtoTransformer :
                 count++
             }
             countryDescriptionItemDto.languages = listOfLanguagesDto
+            listCountryDescriptionItemDto.add(countryDescriptionItemDto)
         }
-        return countryDescriptionItemDto
+
+        return listCountryDescriptionItemDto
+
     }
+
+
 }
