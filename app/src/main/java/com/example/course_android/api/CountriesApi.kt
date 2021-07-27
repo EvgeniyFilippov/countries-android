575482356a0df3766a395485f9703f7b0d8e444d
Yauhen Filipau <6479556@gmail.com>
2021-07-27 17:36:46 +0300

    update

--- app/src/main/java/com/example/course_android/api/CountriesApi.kt
+++ app/src/main/java/com/example/course_android/api/CountriesApi.kt
@@ -1,11 +1,11 @@
 package com.example.course_android.api
 
 import com.example.course_android.Constants.SERVER_API
-import com.example.course_android.model.allCountries.CountriesDataItem
+import com.example.course_android.model.oneCountry.CountryDescriptionItem
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.GET
 
 interface CountriesApi {
     @GET(SERVER_API)
-    fun getTopHeadlines(): Flowable<List<CountriesDataItem>>
+    fun getTopHeadlines(): Flowable<MutableList<CountryDescriptionItem>>
 }
