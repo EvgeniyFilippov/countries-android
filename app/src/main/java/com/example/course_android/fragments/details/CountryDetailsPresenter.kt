2bb442a3a347b789494769e640fc38b0cd712887
Yauhen Filipau <6479556@gmail.com>
2021-07-27 11:02:52 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
@@ -1,20 +1,10 @@
 package com.example.course_android.fragments.details
 
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.course_android.CountriesApp
-import com.example.course_android.R
 import com.example.course_android.api.CountryDescriptionApi
 import com.example.course_android.api.RetrofitObj
 import com.example.course_android.base.mvp.BaseMvpPresenter
 import com.example.course_android.dto.CountryDetailsDtoTransformer
-import com.example.course_android.dto.model.LanguageOfOneCountryDto
-import com.example.course_android.model.oneCountry.CountryDescriptionItem
-import com.example.course_android.utils.loadSvg
-import com.google.android.libraries.maps.SupportMapFragment
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_country_details.*
 
 
 class CountryDetailsPresenter : BaseMvpPresenter<CountryDetailsView>() {
