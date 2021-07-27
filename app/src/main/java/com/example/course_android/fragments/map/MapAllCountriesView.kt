7609ce6339cba5cc55e2d89b120cef4addcfcb80
Yauhen Filipau <6479556@gmail.com>
2021-07-27 17:38:05 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesView.kt
+++ app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesView.kt
@@ -1,9 +1,9 @@
 package com.example.course_android.fragments.map
 
 import com.example.course_android.base.mvp.BaseMvpView
-import com.example.course_android.model.allCountries.CountriesDataItem
+import com.example.course_android.dto.model.CountryDescriptionItemDto
 
 interface MapAllCountriesView : BaseMvpView {
 
-    fun showAllCountriesOnMap(listOfCountries: List<CountriesDataItem>)
+    fun showAllCountriesOnMap(listOfCountries: List<CountryDescriptionItemDto>)
 }
