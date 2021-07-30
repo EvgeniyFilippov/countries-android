b7d0b65226d040154c99a6f40768ca45a624b8d9
Yauhen Filipau <6479556@gmail.com>
2021-07-30 04:25:11 +0300

    update

--- app/src/main/java/com/example/course_android/CountriesApp.kt
+++ app/src/main/java/com/example/course_android/CountriesApp.kt
@@ -1,8 +1,6 @@
 package com.example.course_android
 
 import android.app.Application
-import com.example.course_android.adapters.AdapterLanguages
-import com.example.course_android.adapters.AdapterOfAllCountries
 import com.example.course_android.api.RetrofitObj
 import com.example.course_android.room.CountryInfoDAO
 import com.example.course_android.room.DatabaseInfo
@@ -12,7 +10,7 @@ import retrofit2.Retrofit
 class CountriesApp : Application() {
 
     companion object {
-        lateinit var retrofit: Retrofit
+//        lateinit var retrofit: Retrofit
         var base: DatabaseInfo? = null
         var daoCountry: CountryInfoDAO? = null
         var daoLanguage: LanguagesInfoDAO? = null
@@ -20,7 +18,7 @@ class CountriesApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        retrofit = RetrofitObj.getRetrofit(RetrofitObj.getOkHttp())
+//        retrofit = RetrofitObj.retrofitInit
         base = this.let { DatabaseInfo.init(it) }
         daoCountry = base?.getCountryInfoDAO()
         daoLanguage = base?.getLanguageInfoDAO()
