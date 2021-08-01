b4b59ab342a3b121c61fbfafa000072b8a0b3ba5
Yauhen Filipau <6479556@gmail.com>
2021-08-01 16:54:53 +0300

    update

--- app/src/main/java/com/example/course_android/utils/Sorting.kt
+++ app/src/main/java/com/example/course_android/utils/Sorting.kt
@@ -1,18 +1,19 @@
 package com.example.course_android.utils
 
+import androidx.lifecycle.MutableLiveData
 import com.example.course_android.Constants
 import com.example.course_android.dto.model.CountryDescriptionItemDto
 
-fun MutableList<CountryDescriptionItemDto>.sortBySortStatusFromPref(sortStatus: Int) {
+fun MutableLiveData<MutableList<CountryDescriptionItemDto>>.sortBySortStatusFromPref(sortStatus: Int) {
     when (sortStatus) {
         Constants.SORT_STATUS_UP -> {
-            this.sortBy { it.area }
+            this.value?.sortBy { it.area }
         }
         Constants.SORT_STATUS_DOWN -> {
-            this.sortByDescending { it.area }
+            this.value?.sortByDescending { it.area }
         }
         else -> {
-            this.sortBy {it.name}
+            this.value?.sortBy {it.name}
         }
     }
 }
