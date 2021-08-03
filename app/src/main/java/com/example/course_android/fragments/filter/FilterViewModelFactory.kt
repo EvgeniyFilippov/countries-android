9e98cf6af81a58c121524280cae41fc734c4c854
Yauhen Filipau <6479556@gmail.com>
2021-08-03 16:47:25 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/filter/FilterViewModelFactory.kt
+++ app/src/main/java/com/example/course_android/fragments/filter/FilterViewModelFactory.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.ViewModelProvider
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 class FilterViewModelFactory(
-    private var start:  Float,
-    private val end: Float
+//    private var start:  Float,
+//    private val end: Float
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(FilterViewModel::class.java)) {
-            return FilterViewModel(start, end) as T
+            return FilterViewModel() as T
         }
         throw IllegalArgumentException("Error class. Get ${modelClass.canonicalName}, FilterViewModel")
     }
