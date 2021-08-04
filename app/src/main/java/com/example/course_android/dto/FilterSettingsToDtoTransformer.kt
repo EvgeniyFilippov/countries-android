a2baeedafcf23c8eb9c4c06edc5030506577ae07
Yauhen Filipau <6479556@gmail.com>
2021-08-05 01:09:10 +0300

    update

--- app/src/main/java/com/example/course_android/dto/FilterSettingsToDtoTransformer.kt
+++ app/src/main/java/com/example/course_android/dto/FilterSettingsToDtoTransformer.kt
@@ -2,7 +2,9 @@ package com.example.course_android.dto
 
 import com.example.course_android.Constants.DEFAULT_INT
 import com.example.course_android.Constants.END_AREA_FILTER_KEY
+import com.example.course_android.Constants.END_DISTANCE_FILTER_KEY
 import com.example.course_android.Constants.START_AREA_FILTER_KEY
+import com.example.course_android.Constants.START_DISTANCE_FILTER_KEY
 
 class FilterSettingsToDtoTransformer :
     Transformer<HashMap<String?, Int>, HashMap<String, Int>> {
@@ -15,6 +17,10 @@ class FilterSettingsToDtoTransformer :
             item?.get(START_AREA_FILTER_KEY) ?: DEFAULT_INT
         mapSettingsByFilterDto[END_AREA_FILTER_KEY] =
             item?.get(END_AREA_FILTER_KEY) ?: DEFAULT_INT
+        mapSettingsByFilterDto[START_DISTANCE_FILTER_KEY] ?:
+        item?.get(START_DISTANCE_FILTER_KEY) ?: DEFAULT_INT
+        mapSettingsByFilterDto[END_DISTANCE_FILTER_KEY] ?:
+        item?.get(END_DISTANCE_FILTER_KEY) ?: DEFAULT_INT
 
         return mapSettingsByFilterDto
     }
