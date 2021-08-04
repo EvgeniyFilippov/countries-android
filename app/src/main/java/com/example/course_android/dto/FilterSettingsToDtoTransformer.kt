70b8e2240ff8d0d3bca0c8d4fc4b6210a33e58cd
Yauhen Filipau <6479556@gmail.com>
2021-08-04 13:30:48 +0300

    update

---
+++ app/src/main/java/com/example/course_android/dto/FilterSettingsToDtoTransformer.kt
@@ -0,0 +1,21 @@
+package com.example.course_android.dto
+
+import com.example.course_android.Constants.DEFAULT_INT
+import com.example.course_android.Constants.END_AREA_FILTER_KEY
+import com.example.course_android.Constants.START_AREA_FILTER_KEY
+
+class FilterSettingsToDtoTransformer :
+    Transformer<HashMap<String?, Int>, HashMap<String, Int>> {
+
+    override fun transform(item: HashMap<String?, Int>?): HashMap<String, Int> {
+
+        val mapSettingsByFilterDto: HashMap<String, Int> = hashMapOf()
+
+        mapSettingsByFilterDto[START_AREA_FILTER_KEY] =
+            item?.get(START_AREA_FILTER_KEY) ?: DEFAULT_INT
+        mapSettingsByFilterDto[END_AREA_FILTER_KEY] = item?.get(END_AREA_FILTER_KEY) ?: DEFAULT_INT
+
+        return mapSettingsByFilterDto
+    }
+
+}
