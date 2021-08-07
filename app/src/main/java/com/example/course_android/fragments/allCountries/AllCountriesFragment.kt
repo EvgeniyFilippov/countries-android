e871fb25d73d4d3f993389d0a0497ff3ee643a41
Yauhen Filipau <6479556@gmail.com>
2021-08-07 03:44:50 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/allCountries/AllCountriesFragment.kt
+++ app/src/main/java/com/example/course_android/fragments/allCountries/AllCountriesFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
@@ -47,7 +48,7 @@ class AllCountriesFragment : Fragment(R.layout.fragment_all_countries), BaseMvvm
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel =
-            ViewModelProvider(this, AllCountriesViewModelFactory(sortStatus, mSearchSubject))
+            ViewModelProvider(this, AllCountriesViewModelFactory(sortStatus, mSearchSubject, SavedStateHandle()))
                 .get(AllCountriesViewModel::class.java)
         viewModel.getSearchSubject()
     }
