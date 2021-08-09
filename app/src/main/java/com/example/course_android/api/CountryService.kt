67ed7d0f1ee2a06a0dc4866ea921c465607abaf7
Yauhen Filipau <6479556@gmail.com>
2021-08-10 02:21:58 +0300

    update

--- app/src/main/java/com/example/course_android/api/CountryService.kt
+++ app/src/main/java/com/example/course_android/api/CountryService.kt
@@ -1,18 +1,20 @@
 package com.example.course_android.api
 
+import com.example.course_android.Constants.API_PATH_VALUE
 import com.example.course_android.Constants.SERVER_API
+import com.example.course_android.Constants.SERVER_API_DESCRIPTION
 import com.example.course_android.model.oneCountry.CountryDescriptionItem
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface CountriesApi {
+interface CountryService {
 
     @GET(SERVER_API)
     fun getListOfCountry(): Flowable<MutableList<CountryDescriptionItem>>
 
-    @GET("rest/v2/name/{country}")
+    @GET(SERVER_API_DESCRIPTION)
     fun getCountryDetails(
-        @Path("country") country: String
+        @Path(API_PATH_VALUE) country: String
     ): Flowable<MutableList<CountryDescriptionItem>>
 }
