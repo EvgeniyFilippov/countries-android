635e0001a0ab5d12896a8f97423e04421c047ac4
Yauhen Filipau <6479556@gmail.com>
2021-08-10 15:15:42 +0300

    update

--- app/src/main/java/com/example/course_android/di/AppModule.kt
+++ app/src/main/java/com/example/course_android/di/AppModule.kt
@@ -2,6 +2,10 @@ package com.example.course_android.di
 
 import com.example.course_android.api.RetrofitObj
 import com.example.course_android.room.DatabaseInfo
+import com.repository.database.DatabaseCountryRepository
+import com.repository.database.DatabaseCountryRepositoryImpl
+import com.repository.database.DatabaseLanguageRepository
+import com.repository.database.DatabaseLanguageRepositoryImpl
 import com.repository.network.NetworkRepository
 import com.repository.network.NetworkRepositoryImpl
 import org.koin.dsl.module
@@ -10,8 +14,11 @@ val appModule = module {
 
     //Model level
     single { DatabaseInfo.init(get()) }
-    single { RetrofitObj.getCountriesApi()}
+    single { RetrofitObj.getCountriesApi() }
+
     //Data level
-    single { NetworkRepositoryImpl(get()) as NetworkRepository}
+    single { NetworkRepositoryImpl(get()) as NetworkRepository }
+    single { DatabaseCountryRepositoryImpl(get()) as DatabaseCountryRepository }
+    single { DatabaseLanguageRepositoryImpl(get()) as DatabaseLanguageRepository }
 
 }
