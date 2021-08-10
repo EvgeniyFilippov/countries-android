8e30975d21c822138cfed197a99cf669d76034d0
Yauhen Filipau <6479556@gmail.com>
2021-08-10 14:45:35 +0300

    update

--- app/src/main/java/com/repository/database/DatabaseLanguageRepository.kt
+++ app/src/main/java/com/repository/database/DatabaseLanguageRepository.kt
@@ -3,12 +3,12 @@ package com.repository.database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.course_android.dto.model.LanguageOfOneCountryDto
 import com.example.course_android.room.LanguagesInfoEntity
 import io.reactivex.rxjava3.core.Flowable
 
-interface DatabaseRepository {
+interface DatabaseLanguageRepository {
 
-    /**language*/
     fun add(entity: LanguagesInfoEntity)
 
     fun getAllInfo(): Flowable<List<LanguagesInfoEntity>>
@@ -16,5 +16,4 @@ interface DatabaseRepository {
     fun getLanguageByCountry(name: String): List<String>
 
     fun addAll(list: List<LanguagesInfoEntity>)
-
 }
