b7dad8bdbd51cb09b6ad509917e4ae1f906844c2
Yauhen Filipau <6479556@gmail.com>
2021-08-10 16:11:34 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
+++ app/src/main/java/com/example/course_android/fragments/details/CountryDetailsPresenter.kt
@@ -3,15 +3,18 @@ package com.example.course_android.fragments.details
 import com.example.course_android.api.RetrofitObj
 import com.example.course_android.base.mvp.BaseMvpPresenter
 import com.example.course_android.dto.transformCountryToDto
+import com.repository.network.NetworkRepository
 
-class CountryDetailsPresenter : BaseMvpPresenter<CountryDetailsView>() {
+class CountryDetailsPresenter(
+    private val mNetworkRepository: NetworkRepository
+) : BaseMvpPresenter<CountryDetailsView>() {
 
     fun getMyData(mCountryName: String, isRefresh: Boolean) {
         addDisposable(
             inBackground(
-                handleProgress(RetrofitObj.getCountriesApi().getCountryDetails(mCountryName), isRefresh)
+                handleProgress(mNetworkRepository.getCountryDetails(mCountryName), isRefresh)
             ).subscribe({ response ->
-                getView()?.showCountryInfo( response.transformCountryToDto())
+                getView()?.showCountryInfo( response)
             }, {
                 it.message?.let { it1 -> getView()?.showError(it1, it) }
             })
