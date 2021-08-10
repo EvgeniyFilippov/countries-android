01c5136738820c79682109c5fbfa0de75579dd74
Yauhen Filipau <6479556@gmail.com>
2021-08-10 23:13:13 +0300

    update

--- app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesPresenter.kt
+++ app/src/main/java/com/example/course_android/fragments/map/MapAllCountriesPresenter.kt
@@ -1,8 +1,6 @@
 package com.example.course_android.fragments.map
 
-import com.example.course_android.api.RetrofitObj
 import com.example.course_android.base.mvp.BaseMvpPresenter
-import com.example.course_android.dto.transformCountryToDto
 import com.repository.network.NetworkRepository
 
 class MapAllCountriesPresenter(
