0f70c173a238e8019d12b6be6ad595ed9b247675
yauhen filipau <6479556@gmail.com>
2021-08-17 12:52:21 +0300

    fix constants

--- app/src/main/java/com/example/course_android/Constants.kt
+++ app/src/main/java/com/example/course_android/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.course_android
 
 object Constants {
-    const val BASE_URL = "https://restcountries.eu/"
     const val DEFAULT_INT = 0
     const val KEY_SORT_STATUS = "sortStatus"
     const val FILE_NAME_PREF = "data"
@@ -11,19 +10,7 @@ object Constants {
     const val DEFAULT_SORT_STATUS = 0
     const val ERROR = "ERROR"
     const val COUNTRY_NAME_KEY = "COUNTRY_NAME_KEY"
-    const val COUNTRY_AREA_START_KEY = "COUNTRY_NAME_KEY"
-    const val COUNTRY_AREA_END_KEY = "COUNTRY_NAME_KEY"
-    const val DISTANCE_KEY = "DISTANCE_KEY"
-    const val DEFAULT_FLAG = "https://static.wikia.nocookie.net/science/images/2/2a/Flag_of_None.svg/revision/latest?cb=20091012151312&path-prefix=ru"
     const val DEFAULT_DOUBLE = 0.0
-    const val FIRST_COUNTRY = 0
-    const val DEFAULT_LATLNG_SIZE = 2
-    const val FIRST_ELEMENTS_DB = 20
-    const val SERVER_API = "rest/v2/all"
-    const val SERVER_API_DESCRIPTION = "rest/v2/name/{country}"
-    const val API_PATH_VALUE = "country"
-    const val DEFAULT_SLEEP = 500L
-    const val MARKER_TITLE = "Marker title with position"
     const val MIN_SEARCH_STRING_LENGTH = 3
     const val DEBOUNCE_TIME_MILLIS = 500L
 
@@ -35,19 +22,12 @@ object Constants {
     const val START_POPULATION_FILTER_KEY = "START_POPULATION_FILTER_KEY"
     const val END_POPULATION_FILTER_KEY = "END_POPULATION_FILTER_KEY"
 
-
-
-
     //CONFIG FILTER
     const val FILTER_VALUE_FROM_KEY_AREA = "FILTER_VALUE_FROM_KEY"
     const val FILTER_VALUE_TO_KEY_AREA = "FILTER_VALUE_TO_KEY"
     const val FILTER_VALUE_FROM_KEY_POPULATION = "FILTER_VALUE_FROM_KEY_POPULATION"
     const val FILTER_VALUE_TO_KEY_POPULATION = "FILTER_VALUE_TO_KEY_POPULATION"
     const val VALUE_OF_FILTER_KEY = "VALUE_OF_FILTER_KEY"
-
-
-    const val FILTER_VALUE_START_KEY = 0.0F
-    const val FILTER_VALUE_END_KEY = 0.0F
 
     //LIVE DATA
     const val ALL_COUNTRIES_LIVE_DATA = "allCountriesLiveData"
