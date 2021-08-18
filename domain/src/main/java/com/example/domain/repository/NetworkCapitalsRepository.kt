c3ae9297cdc669060ff9369e2fa8fb3cf056967d
yauhen filipau <6479556@gmail.com>
2021-08-18 19:06:11 +0300

    add coroutines

--- domain/src/main/java/com/example/domain/repository/NetworkCapitalsRepository.kt
+++ domain/src/main/java/com/example/domain/repository/NetworkCapitalsRepository.kt
@@ -1,8 +1,6 @@
 package com.example.domain.repository
 
 import com.example.domain.dto.model.CapitalItemDto
-import com.example.domain.dto.model.CountryDescriptionItemDto
-import io.reactivex.rxjava3.core.Flowable
 
 interface NetworkCapitalsRepository {
 
