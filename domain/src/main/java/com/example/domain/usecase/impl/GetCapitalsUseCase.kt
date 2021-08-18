1775dcf9b3bfae947c8732d40d7c3688bc7ed2d3
yauhen filipau <6479556@gmail.com>
2021-08-18 19:05:37 +0300

    add coroutines

--- domain/src/main/java/com/example/domain/usecase/impl/GetCapitalsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/impl/GetCapitalsUseCase.kt
@@ -1,14 +1,11 @@
 package com.example.domain.usecase.impl
 
 import com.example.domain.dto.model.CapitalItemDto
-import com.example.domain.dto.model.CountryDescriptionItemDto
 import com.example.domain.repository.NetworkCapitalsRepository
-import com.example.domain.repository.NetworkRepository
-import com.example.domain.usecase.UseCase
 import com.example.domain.usecase.UseCaseCoroutines
-import io.reactivex.rxjava3.core.Flowable
 
-class GetCapitalsUseCase(private val networkCapitalsRepository: NetworkCapitalsRepository) : UseCaseCoroutines<Unit, MutableList<CapitalItemDto>>() {
+class GetCapitalsUseCase(private val networkCapitalsRepository: NetworkCapitalsRepository) :
+    UseCaseCoroutines<Unit, MutableList<CapitalItemDto>>() {
 
     override suspend fun buildResult(params: Unit?): MutableList<CapitalItemDto> =
         networkCapitalsRepository.getListOfCapitals()
