a77e27f1b09fdb4101266517f5fee9c83f4645c2
yauhen filipau <6479556@gmail.com>
2021-08-18 16:40:58 +0300

    add capital list function

--- app/src/main/java/com/example/course_android/di/AppModule.kt
+++ app/src/main/java/com/example/course_android/di/AppModule.kt
@@ -1,13 +1,15 @@
 package com.example.course_android.di
 
 import com.example.data.api.RetrofitObj
+import com.example.data.repository.database.DatabaseCountryRepositoryImpl
+import com.example.data.repository.database.DatabaseLanguageRepositoryImpl
+import com.example.data.repository.network.NetworkCapitalRepositoryImpl
+import com.example.data.repository.network.NetworkRepositoryImpl
 import com.example.data.room.DatabaseInfo
 import com.example.domain.repository.DatabaseCountryRepository
-import com.example.data.repository.database.DatabaseCountryRepositoryImpl
 import com.example.domain.repository.DatabaseLanguageRepository
-import com.example.data.repository.database.DatabaseLanguageRepositoryImpl
+import com.example.domain.repository.NetworkCapitalsRepository
 import com.example.domain.repository.NetworkRepository
-import com.example.data.repository.network.NetworkRepositoryImpl
 import org.koin.dsl.module
 
 val appModule = module {
@@ -18,6 +20,7 @@ val appModule = module {
 
     //Data level
     single { NetworkRepositoryImpl(get()) as NetworkRepository }
+    single { NetworkCapitalRepositoryImpl(get()) as NetworkCapitalsRepository }
     single { DatabaseCountryRepositoryImpl(get()) as DatabaseCountryRepository }
     single { DatabaseLanguageRepositoryImpl(get()) as DatabaseLanguageRepository }
 
