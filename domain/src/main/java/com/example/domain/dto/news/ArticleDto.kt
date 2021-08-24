cc328fb0bbae1fbf6986556dbb3339ea8f6c06ad
yauhen filipau <6479556@gmail.com>
2021-08-25 01:55:10 +0300

    add newsmodel

---
+++ domain/src/main/java/com/example/domain/dto/news/ArticleDto.kt
@@ -0,0 +1,12 @@
+package com.example.domain.dto.news
+
+data class ArticleDto(
+    val author: String = "",
+//    val content: Any = "",
+    val description: String = "",
+    val publishedAt: String = "",
+//    val source: Source?,
+    val title: String = "",
+    val url: String = "",
+    val urlToImage: String = ""
+)
