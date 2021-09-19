723e418a2eaa0ff53e7c43375a5ad3e63e3f2537
yauhen filipau <6479556@gmail.com>
2021-09-20 01:28:16 +0300

    add recycler animation

--- app/src/main/java/com/example/course_android/adapters/AdapterCapitals.kt
+++ app/src/main/java/com/example/course_android/adapters/AdapterCapitals.kt
@@ -3,7 +3,9 @@ package com.example.course_android.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.course_android.R
 import com.example.course_android.base.adapter.BaseAdapter
@@ -14,6 +16,7 @@ class AdapterCapitals : BaseAdapter<CapitalItemDto>() {
     class CapitalViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val tvCapital: AppCompatTextView = view.findViewById(R.id.item_capital)
+        val cardTvCapital: CardView = view.findViewById(R.id.item_capital_layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CapitalViewHolder {
@@ -26,6 +29,8 @@ class AdapterCapitals : BaseAdapter<CapitalItemDto>() {
         if (holder is CapitalViewHolder) {
             val item = mDataList[position]
             holder.tvCapital.text = item.capital
+            holder.cardTvCapital.animation =
+                AnimationUtils.loadAnimation(holder.itemView.context, R.anim.recycler_animation)
         }
     }
 
